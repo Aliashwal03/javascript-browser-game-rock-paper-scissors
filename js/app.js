fd20a68933c2cd8f1a69bb41c167f672a4bd169a
Ali Alashawal <aliashwal03@gmail.com>
2025-06-29 10:46:07 +0300

    till setting the backend choices

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/*-------------------------------- Constants --------------------------------*/
+const choices=['paper','rock', 'scssor']
+
+
+/*-------------------------------- Variables --------------------------------*/
+let msg
+let playerChoice
+let computerChoice
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+const rockBtnEL =document.querySelector ('rock')
+const PapkBtnEL =document.querySelector ('paper')
+const ScikBtnEL =document.querySelector ('scissor')
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
